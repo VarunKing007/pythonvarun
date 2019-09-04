f799e4f84dd93dd8519215dc830792f7435a8705
VarunKing007 <54899828+VarunKing007@users.noreply.github.com>
2019-09-04 20:08:54 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,92 @@
+#print("1\n"*100)
+
+#for i in range(1, 16):
+     #print(i)
+
+#range(begin, end+1, step)
+#for variablename in iterable:
+     #statements
+
+#while condition:
+     #statements
+
+#if condition:
+     #statements
+
+#n=int(input("Please give a number !!!! "))#"89"
+#maikel jaksaan
+#if (n % 3)== 0:
+    #print("n is divisible by 3")
+#elif (n%3 == 1):
+    #print("ngives rem 1")
+#else:
+    #print("n giving rem 2")
+
+#if(a==b):
+    #print(equal)
+#if(a+1==b)    :
+    #print(1kadifference)
+#------------------------------------------
+#if(a==b):
+    #same as above
+#elif(a+1==b)    :
+    #print(1kadifference)
+    
+
+#a % b = the remainder when a is divided by b
+
+
+
+
+#-------------------------------------------------------------------------------
+
+#CALUCULATor ALPHA
+#print ("Hello welcome to 'ALPHA' Varun's first caluculator program!!")
+#print ("What do you want me to do?\n")
+# k = function
+#k = int(input("1)Addition\n2)Subtraction\n3)Multiplication\n4)Division "))
+#a = int(input("Please give me your first number!"))
+#b = int(input("Please give me your second number!"))        
+#if (k == 1):
+   # print ("Your answer is " + str(a+b))
+#elif (k == 2):
+   # print ("Your answer is " + str(a-b))
+#elif (k == 3):
+   # print ("Your answer is " + str(a*b))
+#else:
+   # print ("Your answer is " + str(a/b))
+
+#print ("\nHope you got your answer right!! Thank you and please try me again!!!")
+#-------------------------------------------------------------------------------
+#FizzBuzz
+#Output:
+#1 2 Fizz 4 Buzz Fizz ...
+#Every 3 will b fizz 5th will be buzz 15th will be fizzbuzz
+#Prime Number
+#Given n determine if it is prime
+HEHEHEHEHEHEHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+#FIZZBUZZ program
+print ("This is a Fizz Buzz program")
+i = range(1,20)
+if (i%3 == 0 ):
+    print ("Fizz")
+elif (i%5 == 0):
+    print("Buzz")
+else:
+    print (i)
+
+#------------------------------------------------------------------------------------
+
+#Prime numbers
+#print ("Give a number and we will determine if it is a Prime number or not!")
+#n = int(input("Please give a number\n"))
+#if (n % k != 0):
+   # print ("It is a prime number!")
+#else:
+  #  print ("It is not a prime number")
+        
+
+
+
+
+                                                                                                                                                                                                                                                                                                               
